07105e1e4d050a77af44154d934f8f360bfbfc53
Jefry Paul <jefrypaul73@gmail.com>
2025-10-27 03:10:09 +0530

    Pages and Components are created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import Header from "./Components/Header"
+import Admin from "./Pages/Admin" 
+import History from "./Pages/History"
+import LandingPage from "./Pages/LandingPage"
+import Preview from "./Pages/Preview"
+import SelectMedicines from "./Pages/SelectMedicines"
+import Userview from "./Pages/Userview"
 
 function App() {
 
   return (
     <>
-      <h1>Hello</h1>
+      <h1>Hello</h1> 
+      <Header /> 
+      <Admin /> 
+      <History /> 
+      <LandingPage /> 
+      <Preview /> 
+      <SelectMedicines /> 
+      <Userview />
     </>
   )
 }
